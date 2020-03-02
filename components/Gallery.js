f04c54e600fd981419a9f80f9f8d9593fc95bd0a
vinesma <otaviocos_13@hotmail.com>
2020-03-02 09:59:01 -0300

    Gallery.js now renders the images (previews only)
    - Add useEffect for updating and cleanup
    - Add Flatlist for displaying the images

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, Image} from 'react-native';
 import axios from 'axios';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default function Gallery({ route }) {
     const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
 
     const { searchString } = route.params;
 
-    function splitSearchString(){
+    function splitSearchString() {
         return searchString.split(' ').join('+').toLowerCase();
     }
 
-    function getPosts(){
-
-        axios.get(`https://testbooru.donmai.us/posts.json?tags=${splitSearchString()}`)
+    function getPosts() {
+        axios.get(`https://testbooru.donmai.us/posts.json?page=${page}&tags=${splitSearchString()}`)
             .then(res => {
                 setPosts(res.data);
                 console.log('Fetched!');
@@ -21,17 +22,25 @@ export default function Gallery({ route }) {
             .catch(err => console.error(`Error in API call to Danbooru posts : ${err}`));
     }
 
-    function seePosts(){
-        console.log(`searchString : ${splitSearchString()}`);
-        posts.map(elem => console.log(elem.tag_string));
+    function cleanPosts() {
+        setPosts([]);
+        setPage(1);
     }
 
+    useEffect(() => {
+        getPosts();
+
+        return () => cleanPosts();
+    }, [page]);
+
+    const extractKey = ({ id }) => id;
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.bigText}>danviewer</Text>
-            <Button title="Fetch" onPress={() => getPosts()}/>
-            <Button title="See" onPress={() => seePosts()}/>
-        </View>
+        <FlatList
+        style={styles.container}
+        data={posts}
+        keyExtractor={extractKey}
+        renderItem={({item}) => <Image style={styles.image} source={{ uri: item.preview_file_url }}></Image>} />
     )
 }
 
@@ -40,10 +49,14 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 10,
         backgroundColor: '#1f202c',
-        alignItems: 'center',
     },
     bigText: {
         fontSize: 24,
         color: '#CC9B81',
+    },
+    image: {
+        width: 300,
+        height: 300,
+        alignSelf: 'center',
     },
 });
